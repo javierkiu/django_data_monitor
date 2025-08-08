c829e324607d19dbabd2ee0c8753624d9cbf773f
Javier Rodríguez <81196360+javierkiu@users.noreply.github.com>
2025-08-08 04:24:51 +0000

    colocando algunos datos de la landing api en el dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,23 +14,28 @@ def index(request):
 
     # Número total de respuestas
     total_responses = len(posts)
-    # for order_id, order_info in posts.items():
-    #     print(f"ID de Orden: {order_id}")
-    #     print(f"Fecha de la orden: {order_info['timestamp']}")
-    #     print(f"Total de la orden: ${order_info['total']}")
+    order_sum = 0
+    dproductos = {}
+    for order_id, order_info in posts.items():
+        order_sum += order_info['total']
+        # Recorrer los items de cada orden
+        for item_id, item_info in order_info['items'].items():
+            if item_info['name'] in dproductos:
+                dproductos[item_info['name']] += item_info['quantity']
+            else:
+                dproductos[item_info['name']] = item_info['quantity']
         
-    #     # Recorrer los items de cada orden
-    #     for item_id, item_info in order_info['items'].items():
-    #         print(f"  - Producto: {item_info['name']}")
-    #         print(f"    Imagen: {item_info['image']}")
-    #         print(f"    Precio: {item_info['price']}")
-    #         print(f"    Cantidad: {item_info['quantity']}")
-        
-    #     print("\n" + "-"*50 + "\n")
-
+    order_average = order_sum / total_responses if total_responses > 0 else 1
+    max_product = max(dproductos, key=dproductos.get)
+    top10_products = sorted(dproductos.items(), key=lambda x: x[1], reverse=True)[:10]
+    print(top10_products)
     data = {
         'title': "Landing Page' Dashboard",
         'total_responses': total_responses,
+        'order_average': round(order_average, 2), 
+        'max_product': max_product,
+        'orser_sum': round(order_sum, 2),
+        'top10_products': top10_products,
     }
     
     return render(request, 'dashboard/index.html', data)
